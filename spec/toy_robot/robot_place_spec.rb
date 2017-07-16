fc3cdb69c27564b8322ac3ddc3579f9ae1f34ff7
John Bard <johnbard@globalnet.co.uk>
2017-07-16 09:04:22 +0100

    DRY robot place spec

--- spec/toy_robot/robot_place_spec.rb
+++ spec/toy_robot/robot_place_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 RSpec.describe ToyRobot::Robot do
   describe '#place' do
+    let(:table) { ToyRobot::Table.new(5, 7) }
+    let(:facing) { ToyRobot::Direction::WEST }
     before(:each) { robot.place(table, position, facing) }
 
     context 'when placed at position on table' do
       let(:robot) { described_class.new }
-      let(:table) { ToyRobot::Table.new(5, 7) }
       let(:position) { ToyRobot::Point.new(3, 5) }
-      let(:facing) { ToyRobot::Direction::WEST }
       it { expect(robot.table).to eq table }
       it { expect(robot.position).to eq position }
       it { expect(robot.facing).to eq ToyRobot::Direction::WEST }
@@ -16,9 +16,7 @@ RSpec.describe ToyRobot::Robot do
 
     context 'when placed at position not on table' do
       let(:robot) { described_class.new }
-      let(:table) { ToyRobot::Table.new(5, 7) }
       let(:position) { ToyRobot::Point.new(3, 8) }
-      let(:facing) { ToyRobot::Direction::WEST }
       it { expect(robot.table).to be_nil }
       it { expect(robot.position).to be_nil }
       it { expect(robot.facing).to be_nil }
