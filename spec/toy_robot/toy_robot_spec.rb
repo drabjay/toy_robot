829ba1553dfb9b22104bca0b20bb992360394453
John Bard <johnbard@globalnet.co.uk>
2017-07-17 14:53:56 +0100

    DRY toy_robot spec

--- spec/toy_robot/toy_robot_spec.rb
+++ spec/toy_robot/toy_robot_spec.rb
@@ -1,61 +1,34 @@
 require 'spec_helper'
 require 'open3'
 
-# rubocop:disable Metrics/BlockLength
 RSpec.describe 'bin/toy_robot' do
-  describe 'example a' do
-    report = ''
+  subject do
     Open3.popen3('bin/toy_robot') do |i, o|
-      i.puts 'PLACE 0,0,NORTH'
-      i.puts 'MOVE'
-      i.puts 'REPORT'
+      commands.each { |command| i.puts command }
       i.close
-      report = o.gets.chomp
-      o.close
+      o.gets.chomp
     end
-    it { expect(report).to eq '0,1,NORTH' }
+  end
+
+  describe 'example a' do
+    let(:commands) { ['PLACE 0,0,NORTH', 'MOVE', 'REPORT'] }
+    it { expect(subject).to eq '0,1,NORTH' }
   end
 
   describe 'example b' do
-    report = ''
-    Open3.popen3('bin/toy_robot') do |i, o|
-      i.puts 'PLACE 0,0,NORTH'
-      i.puts 'LEFT'
-      i.puts 'REPORT'
-      i.close
-      report = o.gets.chomp
-      o.close
-    end
-    it { expect(report).to eq '0,0,WEST' }
+    let(:commands) { ['PLACE 0,0,NORTH', 'LEFT', 'REPORT'] }
+    it { expect(subject).to eq '0,0,WEST' }
   end
 
   describe 'example c' do
-    report = ''
-    Open3.popen3('bin/toy_robot') do |i, o|
-      i.puts 'PLACE 1,2,EAST'
-      i.puts 'MOVE'
-      i.puts 'MOVE'
-      i.puts 'LEFT'
-      i.puts 'MOVE'
-      i.puts 'REPORT'
-      i.close
-      report = o.gets.chomp
-      o.close
+    let(:commands) do
+      ['PLACE 1,2,EAST', 'MOVE', 'MOVE', 'LEFT', 'MOVE', 'REPORT']
     end
-    it { expect(report).to eq '3,3,NORTH' }
+    it { expect(subject).to eq '3,3,NORTH' }
   end
 
   describe 'example d' do
-    report = ''
-    Open3.popen3('bin/toy_robot') do |i, o|
-      i.puts 'PLACE 1,1,SOUTH'
-      i.puts 'MOVE'
-      i.puts 'REPORT'
-      i.close
-      report = o.gets.chomp
-      o.close
-    end
-    it { expect(report).to eq '1,0,SOUTH' }
+    let(:commands) { ['PLACE 1,1,SOUTH', 'MOVE', 'REPORT'] }
+    it { expect(subject).to eq '1,0,SOUTH' }
   end
 end
-# rubocop:enable Metrics/BlockLength
