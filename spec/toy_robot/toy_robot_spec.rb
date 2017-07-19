6a29e2f155b50700827601b40f685003e4b567ba
John Bard <johnbard@globalnet.co.uk>
2017-07-19 10:15:21 +0100

    Change bin spec describes to contexts

--- spec/toy_robot/toy_robot_spec.rb
+++ spec/toy_robot/toy_robot_spec.rb
@@ -10,24 +10,24 @@ RSpec.describe 'bin/toy_robot' do
     end
   end
 
-  describe 'example a' do
+  context 'when example a' do
     let(:commands) { ['PLACE 0,0,NORTH', 'MOVE', 'REPORT'] }
     it { expect(subject).to eq '0,1,NORTH' }
   end
 
-  describe 'example b' do
+  context 'when example b' do
     let(:commands) { ['PLACE 0,0,NORTH', 'LEFT', 'REPORT'] }
     it { expect(subject).to eq '0,0,WEST' }
   end
 
-  describe 'example c' do
+  context 'when example c' do
     let(:commands) do
       ['PLACE 1,2,EAST', 'MOVE', 'MOVE', 'LEFT', 'MOVE', 'REPORT']
     end
     it { expect(subject).to eq '3,3,NORTH' }
   end
 
-  describe 'example d' do
+  context 'when example d' do
     let(:commands) { ['PLACE 1,1,SOUTH', 'MOVE', 'REPORT'] }
     it { expect(subject).to eq '1,0,SOUTH' }
   end
